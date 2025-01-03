7104b334cf8b9b4982e1fa9eea4a89d475f347f6
ptrzgal <ptrzgal@users.noreply.github.com>
2025-01-03 16:03:05 +0100

    Upload, Start buttons; Selecting in comboBox; displaying PictureBox1

--- EdgeDetectionApp/EdgeDetectionApp/Form1.Designer.cs
+++ EdgeDetectionApp/EdgeDetectionApp/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace EdgeDetectionApp
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace EdgeDetectionApp
 {
     partial class Form1
     {
@@ -49,39 +52,41 @@
             // 
             pictureBox1.Location = new Point(43, 58);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(267, 230);
+            pictureBox1.Size = new Size(300, 300);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Allows you to scale the loaded bitmap to fit the size of the PictureBox control
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(483, 58);
+            pictureBox2.Location = new Point(472, 58);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(267, 230);
+            pictureBox2.Size = new Size(300, 300);
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
             // 
             // button1
             // 
-            button1.Location = new Point(362, 155);
+            button1.Location = new Point(373, 194);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
             button1.Text = "Start";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += Button1_Click; // Link Button1_Click to button1's Click event
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(43, 323);
+            progressBar1.Location = new Point(43, 397);
             progressBar1.Name = "progressBar1";
-            progressBar1.Size = new Size(707, 29);
+            progressBar1.Size = new Size(729, 29);
             progressBar1.TabIndex = 3;
-            progressBar1.Click += progressBar1_Click;
+            progressBar1.Click += ProgressBar1_Click; // Link ProgressBar1_Click to progressBar1's Click event
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(342, 305);
+            label1.Location = new Point(349, 379);
             label1.Name = "label1";
             label1.Size = new Size(117, 15);
             label1.TabIndex = 4;
@@ -92,7 +97,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { uploadToolStripMenuItem, saveToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.Size = new Size(828, 24);
             menuStrip1.TabIndex = 5;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -101,6 +106,7 @@
             uploadToolStripMenuItem.Name = "uploadToolStripMenuItem";
             uploadToolStripMenuItem.Size = new Size(57, 20);
             uploadToolStripMenuItem.Text = "Upload";
+            uploadToolStripMenuItem.Click += UploadToolStripMenuItem_Click; // Link UploadToolStripMenuItem_Click to uploadToolStripMenuItem's Click event
             // 
             // saveToolStripMenuItem
             // 
@@ -111,15 +117,16 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(342, 236);
+            comboBox1.Items.AddRange(new object[] { "C++", "Assembly" });
+            comboBox1.Location = new Point(349, 290);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
+            comboBox1.Size = new Size(117, 23);
             comboBox1.TabIndex = 6;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(379, 218);
+            label2.Location = new Point(386, 272);
             label2.Name = "label2";
             label2.Size = new Size(43, 15);
             label2.TabIndex = 7;
@@ -147,7 +154,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 364);
+            ClientSize = new Size(828, 467);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -160,7 +167,7 @@
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Edge Detection App";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             menuStrip1.ResumeLayout(false);
--- EdgeDetectionApp/EdgeDetectionApp/Form1.cs
+++ EdgeDetectionApp/EdgeDetectionApp/Form1.cs
@@ -7,7 +7,58 @@ namespace EdgeDetectionApp
             InitializeComponent();
         }
 
-        private void progressBar1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Function responsible for the event when “Upload” is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UploadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                // Filter that allows you to select the type of file you are uploading
+                openFileDialog.Filter = "Bitmap Files|*.bmp";
+                openFileDialog.Title = "Select a Bitmap Image";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    // Loading a bitmap and setting it up in pictureBox1
+                    pictureBox1.Image = Image.FromFile(filePath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Function responsible for the event when “Start” is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Get the selected option from the comboBox1 control and convert it to a string. The ? character
+            // indicates that the variable may be null (string?) or prevents exceptions by returning null if
+            // the object to the left of the ?. operator is null.
+            string? selectedOption = comboBox1.SelectedItem?.ToString();
+
+
+            // Depending on the option selected in comboBox1, logic from the appropriate library is selected
+            if (selectedOption == "C++")
+            {
+                // CPP DLL
+            }
+            else if (selectedOption == "Assembly")
+            {
+                // ASM DLL
+            }
+            else
+            {
+                MessageBox.Show("Please choose which library you prefer.");
+            }
+        }
+
+        private void ProgressBar1_Click(object sender, EventArgs e)
         {
 
         }
